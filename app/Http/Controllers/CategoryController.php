38178f62c6b50d6beedb2f3a6f6fbcd15695eb87
IvanAndreevich <ivail89@mail.ru>
2018-07-27 09:38:31 +0300

    Реализован OAuth2 авторизация и методы API

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,9 +14,99 @@ class CategoryController extends Controller
         $this->middleware('auth');
     }
     
+    // Отобрадение всех Категорий для WEB версии
     public function show(){
         $categories = DB::select ("SELECT * FROM `categories`");
         return view('categories',['categories' => $categories]);
+    }
+
+    // Отображение всех продуктоа по API
+    public function apiShow(Request $request){
+        $categories = Category::all();
+        $type = $request->get('type');
+
+        // Массив с данными на выход
+        $json_data = array ();
+        
+        switch ($type){
+            case 'show':
+                foreach ($categories as $category){
+                    array_push($json_data,[
+                            'id' => $category->id,
+                            'name' => $category->name
+                        ]);
+                }
+                break;
+            case 'add':
+                /*
+                 * Для добавления достаточно указать name Имя новой Категории.
+                 * Перед добавлением выполняем проверку, есть ли Категория с таким именем.
+                 */
+                $name = $request->get('name');
+                $add = Category::where('name', '=', $name)->get();
+                if (!$add->count()){
+                    Category::insert(
+                        ['name' => $name]
+                    );
+                } else {
+                    $res = 'Category '.$name.' - exists!';
+                    array_push($json_data, $res);
+                    break;
+                }
+                //Обновляем страницу с новыми данными
+                array_push($json_data, 'Category added');
+                break;
+            case 'delete':
+                /*
+                 * Для удаления достаточно указать ID удаляемой Категории
+                 * Выполняем проверку - существует ли Категория с таким ID
+                 */
+                $id = $request->get('id');
+                $delete = Category::where('id', '=', $id)->get();
+                // Проверям на существование записи в БД
+                if ($delete->count()){
+                    Category::where('id', '=', $id)
+                            ->delete();
+                } else {
+                    $res = 'ID='.$id.' - do not exist!';
+                    array_push($json_data, $res);
+                    break;
+                }
+
+                array_push($json_data, 'Category deleted');
+                break;
+            case 'edit':
+                /*
+                 * Для редактрирования необходимо указать 
+                 * ID редактируемой категории 
+                 * name - новое имя.
+                 * Две проверки:
+                 * 1. Есть ли Категория с таким именем
+                 * 2. Новое имя ранее не встречалось
+                 */
+                $id = $request->get('id');
+                $name = $request->get('name');
+                $edit = Category::where('id', '=', $id)->get();
+                if ($edit->count()){
+                    $edit = Category::where('name', '=', $name)->get();
+                    if (!$edit->count()){
+                        Category::where('id', '=', $id)
+                            ->update(['name' => $name]);
+                    } else {
+                        $res = 'Category '.$name.' - exist with other ID';
+                        array_push($json_data, $res);
+                        break;
+                    }
+                } else {
+                    $res = 'ID='.$id.' - do not exist!';
+                    array_push($json_data, $res);
+                    break;
+                }
+                array_push($json_data, 'Category edited');
+                break;
+        };
+        
+        return response()->json($json_data,201);
     }
     
     // Обновление данных на основании запроса из формы редактирования
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,9 +16,176 @@ class ProductController extends Controller
         $this->middleware('auth');
     }
 
+    // Отобрадение всех продуктов для WEB версии
     public function show(){
         $products = Product::all();
         return view('product',['products' => $products]);
+    }
+    
+    
+    // Отображение всех продуктоа по API
+    public function apiShow(Request $request){
+        $products = Product::all();
+        $type = $request->get('type');
+
+        // Массив с данными на выход
+        $json_data = array ();
+        
+        switch ($type){
+            case 'show':
+                /*
+                 * Для вывода продуктов конкретной категории, необходимо указать 
+                 * category = ID Категории
+                 */
+                $category_id = $request->get('category');
+                if ($category_id){
+                    $categories = Category::with('products')
+                        ->where('id', $category_id)
+                        ->get();
+                    foreach ($categories as $category){
+                        foreach ($category->products as $product){
+                            array_push($json_data,[
+                                    'id' => $product->id,
+                                    'name' => $product->name
+                                ]);
+                        }
+                    }
+                    break;
+                }
+                foreach ($products as $product){
+                    // Массив категорий одного продукта
+                    $arrCategories = array ();
+
+                    // Собираем категории для одного продукта
+                    foreach ($product->categories as $p){
+                        array_push($arrCategories, $p->name);
+                    }
+                    array_push($json_data,[
+                            'id' => $product->id,
+                            'name' => $product->name,
+                            'categories' => $arrCategories
+                        ]);
+                }
+                break;
+            case 'add':
+                /*
+                 * Для добавления необходимо указать 
+                 * name = Имя нового продукта 
+                 * category_id = ID категории товара.
+                 */
+
+                $name = $request->get('name');
+                $category_id = $request->get('category_id');
+                
+                //Данная Категория уже существует?
+                $addCategory = Category::with('products')
+                        ->where('id', $category_id)
+                        ->get();
+                if (!$addCategory->count()){
+                    $res = 'category_id '.$category_id.' - do not exists!';
+                    array_push($json_data, $res);
+                    break;
+                }
+
+                //Данный Продукт уже существует? 
+                $addProduct = Product::with('categories')
+                    ->where('name', $name)
+                    ->get();
+                if ($addProduct->count()){
+                    $res = 'Product '.$name.' - exists!';
+                    array_push($json_data, $res);
+                    break;
+                }
+                
+                // Вставка записей в БД
+                $addProduct = new Product(['name' => $name]);
+                foreach ($addCategory as $aC){
+                    $aC->products()->save($addProduct);
+                }
+                array_push($json_data, 'Product added');
+                break;
+            case 'delete':
+                /*
+                 * Для удаления достаточно указать ID удаляемого Продукта
+                 * Выполняем проверку - существует ли Продукт с таким ID
+                 * Все связи с данным продуктом удаляем из таблицы products_categories
+                 */
+                $id = $request->get('id');
+                $deleteProduct = Product::with('categories')
+                    ->where('id', $id)
+                    ->get();
+                if ($deleteProduct->count()){
+                    foreach ($deleteProduct as $dP){
+                        // получаем экземпляры Категорий, связь с которыми нужно удалить
+                        foreach ($dP->categories as $c){
+                            $deleteCategory_id = Category::find($c->id);
+                            $dP->categories()->detach($deleteCategory_id);
+                        }
+                        // Удаляем запись о продукте
+                        $dP->where('id', '=', $id)->delete();
+                    }
+                } else {
+                    $res = 'ID='.$id.' - do not exist!';
+                    array_push($json_data, $res);
+                    break;
+                }
+               
+                array_push($json_data, 'Product deleted');
+                break;
+            case 'edit':
+                /*
+                 * Для редактрирования необходимо указать
+                 * id - ID редактируемого продукта
+                 * category_id - ID редактируемой Категории 
+                 * name - новое имя Продукта
+                 * Порядок обработки запроса:
+                 * 1. Продукт с таким ID существует? Нет - ошибка
+                 * 2. Проукт существует, отличается имя обновляем имя Продукта
+                 * 3. Имя совпадает, ID Категории добавляем к имеющейся Категории 
+                 */
+                
+                $id = $request->get('id');
+                $name = $request->get('name');
+                $category_id = $request->get('category_id');
+
+                //Данная Категория уже существует?
+                $editCategory = Category::with('products')
+                        ->where('id', $category_id)
+                        ->get();
+                if (!$editCategory->count()){
+                    $res = 'category_id '.$category_id.' - do not exists!';
+                    array_push($json_data, $res);
+                    break;
+                }
+                
+                //ID данного продукта существует?
+                $editID = Product::with('categories')
+                    ->where('id', $_POST['id'])
+                    ->get();
+                if (!$editID->count()){
+                    $res = 'ID='.$id.' - do not exist!';
+                    array_push($json_data, $res);
+                    break;
+                }
+                
+                //Данный Продукт (Имя) уже существует? 
+                $editProduct = Product::with('categories')
+                    ->where('name', $name)
+                    ->get();
+                if ($editProduct->count()){
+                    foreach ($editProduct as $aP){
+                        $aP -> categories() -> attach($category_id);
+                    }
+                    array_push($json_data, 'Category add to Product');
+                } else {
+                    Product::where('id', '=', $id)
+                        ->update(['name' => $name]);
+                    array_push($json_data, 'Product edited');
+                }
+                break;
+        };
+        
+        return response()->json($json_data,201);
     }
 
     // Обновление данных на основании запроса из формы редактирования
